cd6e8e5207b8aae1a4381d70df3b4dd048c90dd1
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-08-25 15:03:06 +0100

    up

--- invista_me/views.py
+++ invista_me/views.py
@@ -16,3 +16,12 @@ def minha_historia(request):
 		'hobby':'rest'
 	}
 	return render(request,'investimentos/minha_historia.html',pessoa)
+
+def novo_investimento(request):
+	return render(request,'investimentos/novo_investimento.html')
+
+def investimento_registrado(request):
+	investimento = {
+		'tipo_investimento':request.POST.get('TipoInvestimento')
+	}
+	return render(request,'investimentos/investimento_registrado.html',investimento)

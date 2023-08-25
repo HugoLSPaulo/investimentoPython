c19f81dbf4730eea4c80173e2de5d9a3e2fcf8bf
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-08-25 13:57:25 +0100

    views

--- invista_me/views.py
+++ invista_me/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def pagina_inicial(request):
+	return HttpResponse('Pronto para investir')
+
+def pagina_de_contacto(request):
+	return HttpResponse('Esta e a pagina de contacto')

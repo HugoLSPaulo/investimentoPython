4c2e4ec6b255ed32121b4c9c007fdf536c4e9e77
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-08-25 15:02:45 +0100

    inve

--- investimentoPython/urls.py
+++ investimentoPython/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.pagina_inicial),
     path('contactos/',views.pagina_de_contacto, name = 'contactos'),
-    path('minha/historia/',views.minha_historia)
+    path('minha/historia/',views.minha_historia),
+    path('novo_investimento',views.novo_investimento, name = 'novo_investimento'),
+    path('investimento_registrado',views.investimento_registrado,name = 'investimento_registrado')
 ]

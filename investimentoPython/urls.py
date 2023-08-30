74c4a25b7d6916a4c0fb8b0b6a6b473f8eed8300
HugoLSPaulo <64653369+HugoLSPaulo@users.noreply.github.com>
2023-08-30 23:57:44 +0100

    up

--- investimentoPython/urls.py
+++ investimentoPython/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('',views.investimentos),
     path('contactos/',views.pagina_de_contacto, name = 'contactos'),
     path('minha/historia/',views.minha_historia),
-    path('novo_investimento',views.novo_investimento, name = 'novo_investimento'),
+    path('novo_investimento',views.criar, name = 'novo_investimento'),
     path('investimento_registrado',views.investimento_registrado,name = 'investimento_registrado'),
     path('/<int:id_investimento>',views.detalhe, name='detalhe')
 ]
